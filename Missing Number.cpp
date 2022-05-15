d4e6d5ca1425499a58bab3808f4f52fb752c3316
Shrishti-2002 <83156164+Shrishti-2002@users.noreply.github.com>
2022-05-15 23:09:20 +0530

    Create Missing Number.cpp

---
+++ Missing Number.cpp
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i) return i;
+        }
+        return nums.size();
+    }
+};
